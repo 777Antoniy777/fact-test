2e18ce042e6ee2ef212bd1177b4a022b2f6a68f4
Anton_Kuzmitsky <tat7106@gmail.com>
2021-01-16 18:01:41 +0600

    add new data components

---
+++ src/components/Data.vue
@@ -0,0 +1,56 @@
+<template>
+  <section class="data">
+    <form class="data__form">
+      <button type="reset">Сбросить</button>
+    </form>
+
+    <!-- Data -->
+    <DataPresentation />
+  </section>
+</template>
+
+<script>
+import DataPresentation from "@/components/DataPresentation.vue";
+
+export default {
+  name: "Data",
+  components: {
+    DataPresentation
+  },
+  props: {
+    data: Array
+  },
+  data() {
+    return {
+      choosenData: []
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.data {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 20px;
+
+  &__form {
+    margin-right: 20px;
+  }
+
+  button {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 150px;
+    margin: 0 auto;
+    padding: 5px 10px;
+    text-align: center;
+    color: white;
+    background-color: orange;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+  }
+}
+</style>
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,15 +1,20 @@
 <template>
   <main class="main">
-    <!-- Loader -->
-    <Loader v-if="isLoading" />
+    <div class="site-wrapper">
+      <!-- Loader -->
+      <Loader v-if="isLoading" />
 
-    <!-- Error message -->
-    <Error v-if="status === 404" />
+      <!-- Error message -->
+      <Error v-if="status === 404" />
 
-    <!-- Time loading -->
-    <p class="main__time" v-if="data.length">Время загрузки: {{ time }} с</p>
+      <!-- Time loading -->
+      <p class="main__time" v-if="data.length">Время загрузки: {{ time }} с</p>
 
-    <pre v-if="data.length">{{ data }}</pre>
+      <!-- <pre v-if="data.length">{{ data }}</pre> -->
+      <div class="main__inner-wrapper" v-if="data.length">
+        <Data :data="data" />
+      </div>
+    </div>
   </main>
 </template>
 
@@ -17,6 +22,7 @@
 import createAPI from "@/api";
 import Loader from "@/components/Loader.vue";
 import Error from "@/components/Error.vue";
+import Data from "@/components/Data.vue";
 
 const api = createAPI();
 
@@ -24,20 +30,21 @@ export default {
   name: "Main",
   components: {
     Loader,
-    Error
+    Error,
+    Data
   },
   mounted() {
     setTimeout(() => {
       api
         .get()
         .then(response => {
+          let result = [];
           let data = response.data.testArr;
-          console.log(data);
-          data = this.flatArr(data);
-          console.log(data);
-          data = this.sortArr(data);
 
-          this.data = data;
+          this.flatArr(data, result);
+          result = this.filterArr(result);
+
+          this.data = result;
           this.status = response.status;
           this.isLoading = false;
           this.time = ((Date.now() - this.time) / 1000).toFixed(1);
@@ -59,84 +66,68 @@ export default {
     };
   },
   methods: {
-    flatArr(arr) {
-      const result = [];
+    flatArr(data, result) {
       const toString = Object.prototype.toString;
 
-      const func = function() {
-        for (let i = 0; i < arr.length; i++) {
-          const elem = arr[i];
-          const type = toString.call(elem);
+      for (let i = 0; i < data.length; i++) {
+        const elem = data[i];
+        const type = toString.call(elem);
 
-          if (type === "[object Array]") {
-            // console.log(22);
-            func(elem);
-          } else {
-            result.push(elem);
-          }
+        if (type === "[object Array]") {
+          this.flatArr(elem, result);
+        } else {
+          result.push(elem);
         }
-      };
-
-      console.log(func);
-
-      // for (let i = 0; i < arr.length; i++) {
-      //   const elem = arr[i];
-      //   const type = toString.call(elem);
-
-      //   if (type === "[object Array]") {
-      //     // console.log(22);
-      //     this.flatArr(elem);
-      //   } else {
-      //     result.push(elem);
-      //   }
-      // }
-
-      console.log(result);
-      return result;
+      }
     },
-    sortArr(arr) {
+    filterArr(data) {
       const map = new Map();
       const result = [];
       const toString = Object.prototype.toString;
 
-      for (let i = 0; i < arr.length; i++) {
-        const elem = arr[i];
+      for (let i = 0; i < data.length; i++) {
+        const elem = data[i];
         const type = toString.call(elem);
-        let simpleValueOfType = type.split(" ")[1].toLowerCase();
-        simpleValueOfType = simpleValueOfType.slice(
+        let simpleNameOfType = type.split(" ")[1].toLowerCase();
+        simpleNameOfType = simpleNameOfType.slice(
           0,
-          simpleValueOfType.length - 1
+          simpleNameOfType.length - 1
         );
 
-        if (map.get(simpleValueOfType)) {
-          const array = map.get(simpleValueOfType);
-          map.set(simpleValueOfType, [...array, elem]);
-        } else {
-          map.set(simpleValueOfType, [elem]);
-        }
+        if (simpleNameOfType !== "null" && simpleNameOfType !== "underfined") {
+          switch (simpleNameOfType) {
+            case "object":
+              if (Object.keys(elem).length) {
+                this.fillMap(map, simpleNameOfType, elem);
+              }
+              break;
 
-        // if (
-        //   ((simpleValueOfType === "object" && Object.keys(elem).length) ||
-        //     (simpleValueOfType === "string" && elem.trim().length)) &&
-        //   simpleValueOfType !== "null" &&
-        //   simpleValueOfType !== "underfined"
-        // ) {
-        //   if (map.get(simpleValueOfType)) {
-        //     const array = map.get(simpleValueOfType);
-        //     map.set(simpleValueOfType, [...array, elem]);
-        //   } else {
-        //     map.set(simpleValueOfType, [elem]);
-        //   }
-        // }
+            case "string":
+              if (elem.trim().length) {
+                this.fillMap(map, simpleNameOfType, elem);
+              }
+              break;
+
+            default:
+              this.fillMap(map, simpleNameOfType, elem);
+              break;
+          }
+        }
       }
 
       for (let value of map.values()) {
         result.push(value);
       }
 
-      // console.log(map);
-      // console.log(result);
       return result;
+    },
+    fillMap(map, type, elem) {
+      if (map.get(type)) {
+        const array = map.get(type);
+        map.set(type, [...array, elem]);
+      } else {
+        map.set(type, [elem]);
+      }
     }
   }
 };
@@ -146,6 +137,10 @@ export default {
 .main {
   &__time {
     font-weight: 700;
+  }
+
+  &__inner-wrapper {
+    // display: flex;
   }
 }
 </style>
